aa264ec2e2432bb42bee2aef55614bcf1fb1183a
Turchynyak <pavlo.turchynyak@infineon.com>
2023-10-15 12:35:06 +0300

    first commit

---
+++ src/main/java/ua/lviv/iot/lab5/domain/Country.java
@@ -0,0 +1,50 @@
+package ua.lviv.iot.lab5.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Country {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "name")
+    private String name;
+    @OneToMany(mappedBy = "country")
+    private List<City> cities;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Country country = (Country) o;
+        return Objects.equals(name, country.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    public List<City> getCities() {
+        return cities;
+    }
+
+    public void setCities(List<City> cities) {
+        this.cities = cities;
+    }
+}
